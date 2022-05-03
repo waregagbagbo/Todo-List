75eb4bbaecda10a58a6a35e71f6518408e098219
waregagbagbo <gabelwarega@gmail.com>
2022-05-03 12:08:35 +0300

    changed the task detail image

--- worklist/forms.py
+++ worklist/forms.py
@@ -11,7 +11,7 @@ class TaskCreationForm(forms.ModelForm):
         widgets = {
             'title':forms.TextInput(attrs={'class':'form-control'}),
             'description':forms.Textarea(attrs={'class':'form-control'}),
-            'complete':forms.CheckboxInput(attrs={'class':''}),       
+            'complete':forms.CheckboxInput(attrs={'class':''}),     
 
         }
 
